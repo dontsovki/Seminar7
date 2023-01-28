00e8ae28fc305839b6f946ecdf8c12797145b94c
Artem.don <dontsovki@gmail.com>
2023-01-28 13:14:55 +0300

    Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.

---
+++ Task4/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Console.Clear();
+
+int [,] matrix = new int[4,4];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+FillArrayMidl(matrix);
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = i + j;
+        }
+    }
+}
+
+void FillArrayMidl(int[,] matr)
+{
+    Console.Write("Среднее: ");
+    for (int i = 0; i < matr.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < matr.GetLength(0); j++)
+        {
+            sum += matr[i,j];
+        }
+        Console.Write($"{sum/matr.GetLength(0)}, ");
+    }
+}
